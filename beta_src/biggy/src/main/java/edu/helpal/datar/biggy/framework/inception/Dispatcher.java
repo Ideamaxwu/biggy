ce5bde74dd4bfd99542261a48b5e08f9cc810488
Ideamaxwu <ideamaxwu@gmail.com>
2018-05-07 19:59:19 +0800

    add thread pool test

--- beta_src/biggy/src/main/java/edu/helpal/datar/biggy/framework/inception/Dispatcher.java
+++ beta_src/biggy/src/main/java/edu/helpal/datar/biggy/framework/inception/Dispatcher.java
@@ -1,5 +1,8 @@
 package edu.helpal.datar.biggy.framework.inception;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * 
  * Dispatcher
@@ -11,12 +14,28 @@ public class Dispatcher {
 	void Dispatch(String engineName, String jobName, String job){
 		//TODO dispatch
 		commandParse(job);
-		dispatchPool();
+		dispatchPoolTest();
 		System.out.println("Dispatching Job " + jobName +" to Engine " + engineName);
 	}
 	
-	void dispatchPool(){
+	void dispatchPoolTest(){
 		//TODO thread pool
+		final int MAX_T = 3;
+		Runnable r1 = new DispatchTask("Task 001");
+		Runnable r2 = new DispatchTask("Task 002");
+		Runnable r3 = new DispatchTask("Task 003");
+		Runnable r4 = new DispatchTask("Task 004");
+		Runnable r5 = new DispatchTask("Task 005");
+		
+		ExecutorService pool = Executors.newFixedThreadPool(MAX_T);
+		
+		pool.execute(r1);
+		pool.execute(r2);
+		pool.execute(r3);
+		pool.execute(r4);
+		pool.execute(r5);
+		
+		pool.shutdown();
 		
 	}
 	
