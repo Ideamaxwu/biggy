a9a149ac3ec963f82dc76ab5f00015b8ad84963a
Ideamaxwu <ideamaxwu@gmail.com>
2018-05-11 11:02:23 +0800

    add datamodel template

---
+++ beta_src/biggy/src/main/java/edu/helpal/datar/biggy/framework/datamodel/BigData.java
@@ -0,0 +1,16 @@
+package edu.helpal.datar.biggy.framework.datamodel;
+
+public class BigData {
+	private BigData bigdata;
+	
+	BigData Action(){
+		
+		return bigdata;
+	}
+	
+	BigData Tansform(){
+		
+		return bigdata;
+	}
+	
+}
---
+++ beta_src/biggy/src/main/java/edu/helpal/datar/biggy/framework/datamodel/DataReactor.java
@@ -0,0 +1,16 @@
+package edu.helpal.datar.biggy.framework.datamodel;
+
+public class DataReactor {
+	
+	void eventRegister(){
+		
+	}
+	
+	void eventListen(){
+		
+	}
+	
+	void eventProcess(){
+		
+	}
+}
