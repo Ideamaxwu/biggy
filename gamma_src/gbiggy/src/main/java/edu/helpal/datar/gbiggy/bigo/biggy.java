8526958b169796493351379019efcb6f416d934e
Ideamaxwu <ideamaxwu@gmail.com>
2018-05-28 16:49:51 +0800

    init gamma version

---
+++ gamma_src/gbiggy/src/main/java/edu/helpal/datar/gbiggy/bigo/biggy.java
@@ -0,0 +1,15 @@
+package edu.helpal.datar.gbiggy.bigo;
+
+/**
+ * @author DWBI 1deamaxwu
+ * 
+ * biggy: All Fit in One, A Unified Framework for Big Data Management System
+ *
+ */
+public class biggy 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello biggy!" );
+    }
+}
