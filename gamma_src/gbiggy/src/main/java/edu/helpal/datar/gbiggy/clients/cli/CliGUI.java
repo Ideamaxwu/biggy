1e378aaefe088fb880afa56bc2d7e3703ed42d28
Ideamaxwu <ideamaxwu@gmail.com>
2018-06-04 17:33:29 +0800

    merge Inception to CliClient

--- gamma_src/gbiggy/src/main/java/edu/helpal/datar/gbiggy/clients/cli/CliGUI.java
+++ gamma_src/gbiggy/src/main/java/edu/helpal/datar/gbiggy/clients/cli/CliGUI.java
@@ -1,12 +1,5 @@
 package edu.helpal.datar.gbiggy.clients.cli;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
 /**
  * 
  * Command CLIent
@@ -16,39 +9,7 @@ import java.net.UnknownHostException;
 public class CliGUI {
 	public static void main(String[] args){
 		System.out.println("biggy CLI working...");
-		Client client = new Client();
+		CliClient client = new CliClient();
 		client.startClient();
-	}
-}
-class Client{
-	private Socket clientSocket;
-
-	public void startClient(){
-		System.out.print("client> ");
-		String host = "localhost";
-		int port = 9091;
-		try{
-			clientSocket = new Socket(host, port);  
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);  
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in)); 
-            String userInput;  
-            while ((userInput = stdIn.readLine()) != null) {  
-            	out.println(userInput);  
-            	System.out.println(in.readLine()); 
-            	System.out.print("client> ");
-            	if(userInput.equalsIgnoreCase("quit")){
-            		System.out.print("client closed.");
-            		clientSocket.close();
-            		break;
-            	}
-            }
-		} catch(UnknownHostException e){
-			System.err.println("Don't know about host " + host);  
-            System.exit(1);
-		} catch(IOException e){
-			System.err.println("Couldn't get I/O for the connection to " + host);  
-            System.exit(1);
-		}
 	}
 }
