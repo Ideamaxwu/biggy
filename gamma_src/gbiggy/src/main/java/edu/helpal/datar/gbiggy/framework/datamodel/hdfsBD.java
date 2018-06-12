6fb4b02a0b192e9069beec8b0ca3dab925786ebe
Ideamaxwu <ideamaxwu@gmail.com>
2018-06-12 14:11:01 +0800

    hbase wordcount task001 template

--- gamma_src/gbiggy/src/main/java/edu/helpal/datar/gbiggy/framework/datamodel/hdfsBD.java
+++ gamma_src/gbiggy/src/main/java/edu/helpal/datar/gbiggy/framework/datamodel/hdfsBD.java
@@ -1,5 +1,18 @@
 package edu.helpal.datar.gbiggy.framework.datamodel;
 
+import edu.helpal.datar.gbiggy.pipelines.WordCount.HBasePipeJobWordCountTask001;
+
 public class hdfsBD {
+	private BigData bigdata;
+
+	public hdfsBD(BigData bigdata) {
+		this.bigdata = bigdata;
+	}
+
+	public void WriteToHBase(String tablename) {
+		
+		System.out.println("Write to HBase Table: " + tablename);
+		new HBasePipeJobWordCountTask001().flush(this.bigdata.content);
+	}
 
 }
